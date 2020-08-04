2ba04e922ea2ce2a66aae4c747f26448c8791c2d
sjcj333 <sjcj333@naver.com>
2020-08-04 14:41:10 +0900

    new start

---
+++ SnakeGame.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <string>
+#include <queue>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello Snake!";
+    return 0;
+}
