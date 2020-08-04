2d1d068aa31721ecaefb8ef4eca04c89513f416c
sjcj333 <sjcj333@naver.com>
2020-08-04 14:43:23 +0900

    Define gotoxy

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -8,6 +8,12 @@
 
 using namespace std;
 
+void gotoxy(short int x, short int y, string z){
+    COORD pos={x,y};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos); 
+    cout << z;
+}
+
 int main()
 {
     cout << "Hello Snake!";
